10c743628e6c8432c30222580230c1c72c651e30
MayankRajeevkumarShukla <97381684+MayankRajeevkumarShukla@users.noreply.github.com>
2025-07-27 22:59:32 +0530

    added git ignore file

--- new_game/guessing_game/src/main.rs
+++ new_game/guessing_game/src/main.rs
@@ -1,3 +1,11 @@
+use std::io;
 fn main() {
-    println!("Hello, world!");
+    println!("Guess a number");
+    println!("Please input your guess");
+
+    let mut guess = string::new();
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read a line")
+    println!("you guessed:{guess}")    
 }
