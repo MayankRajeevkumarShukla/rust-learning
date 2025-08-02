9bc11a432fbd92aa72dd49b25090ff5c8c4f9dc9
MayankRajeevkumarShukla <97381684+MayankRajeevkumarShukla@users.noreply.github.com>
2025-08-03 00:21:17 +0530

    learining

--- structs.rs
+++ structs.rs
@@ -4,3 +4,13 @@ struct User{
     email:String,
     sign_in_count:u64,
 }
+fn main(){
+    let mut user1 = User{
+        activte:true,
+        username:String::from("someusername123"),
+        email:String::from("someone@example.com"),
+        sign_in_count:1,
+
+    };
+    user1.email = String::from("Ag")
+}
