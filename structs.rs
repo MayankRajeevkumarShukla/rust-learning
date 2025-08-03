0518b3b3f3070103c3bbad91f1f2d7dc087ee322
MayankRajeevkumarShukla <97381684+MayankRajeevkumarShukla@users.noreply.github.com>
2025-08-03 20:05:50 +0530

    structs done

--- structs.rs
+++ structs.rs
@@ -38,3 +38,10 @@ fn build_user(email:String,username:String) ->User{
         sign_in_count:1,
     }
 }
+struct Color(i32,i32,i32);
+struct Point(i32,i32,i32);
+
+fn structTuple(){
+    let black = Color(0,0,0);
+    let origin = Point(0,0,0);
+}
