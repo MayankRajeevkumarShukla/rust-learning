23f24653eae98b46583ed18bb930dd27b4501431
MayankRajeevkumarShukla <97381684+MayankRajeevkumarShukla@users.noreply.github.com>
2025-08-04 07:54:34 +0530

    enums done

--- enums.rs
+++ enums.rs
@@ -6,34 +6,44 @@ enum IpAddrKind {
 struct IpAddr {
     kind: IpAddrKind,
     address: String,
+    nickname: Option<String>, // Added Option type here
 }
 
 fn route(ip_kind: IpAddrKind) {
     // function body can be empty for now
 }
 
-enum Message{
+enum Message {
     Quit,
-    Move{x:i32,y:i32},
+    Move { x: i32, y: i32 },
     Write(String),
-    ChangeColor(i32,i32,i32)
+    ChangeColor(i32, i32, i32),
 }
-fn main() {
-    let four = IpAddrKind::V4;
-    let six = IpAddrKind::V6;
 
+fn main() {
     let home = IpAddr {
-        kind: IpAddrKind::V4,
+        kind: IpAddrKind::V4(String::from("127.0.0.1")),
         address: String::from("127.0.0.1"),
+        nickname: Some(String::from("Localhost")), // Using Some variant
     };
-    let home = IpAddr::v4(String::from("127.0.0.1"));
-    
-    
+
     let loopback = IpAddr {
-        kind: IpAddrKind::V6,
+        kind: IpAddrKind::V6(String::from("::1")),
         address: String::from("::1"),
+        nickname: None, // No nickname provided
     };
-    let loopback = IpAddr::v4(String::from("127.0.0.1"));
+
+    // Example usage of Option
+    match &home.nickname {
+        Some(name) => println!("Home nickname: {}", name),
+        None => println!("Home has no nickname"),
+    }
+
+    match &loopback.nickname {
+        Some(name) => println!("Loopback nickname: {}", name),
+        None => println!("Loopback has no nickname"),
+    }
+
     // Example usage of the function
     route(home.kind);
     route(loopback.kind);
