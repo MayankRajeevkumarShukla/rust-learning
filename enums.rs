4bf7565c072f71bee5c0d27f08f239f1ad7e2019
MayankRajeevkumarShukla <97381684+MayankRajeevkumarShukla@users.noreply.github.com>
2025-08-04 07:40:41 +0530

    enums learning

--- enums.rs
+++ enums.rs
@@ -1,8 +1,40 @@
-enum IpAddrKind{
-    V4,
-    V6
+enum IpAddrKind {
+    V4(String),
+    V6(String),
 }
-fn main(){
-    let four =IpAddrKind::V4;
-    let six =IpAddrKind::V6;
+
+struct IpAddr {
+    kind: IpAddrKind,
+    address: String,
+}
+
+fn route(ip_kind: IpAddrKind) {
+    // function body can be empty for now
+}
+
+enum Message{
+    Quit,
+    Move{x:i32,y:i32},
+    Write(String),
+    ChangeColor(i32,i32,i32)
+}
+fn main() {
+    let four = IpAddrKind::V4;
+    let six = IpAddrKind::V6;
+
+    let home = IpAddr {
+        kind: IpAddrKind::V4,
+        address: String::from("127.0.0.1"),
+    };
+    let home = IpAddr::v4(String::from("127.0.0.1"));
+    
+    
+    let loopback = IpAddr {
+        kind: IpAddrKind::V6,
+        address: String::from("::1"),
+    };
+    let loopback = IpAddr::v4(String::from("127.0.0.1"));
+    // Example usage of the function
+    route(home.kind);
+    route(loopback.kind);
 }
