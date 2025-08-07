d47d6ae2917ce4c2549104acbfe6119384b63e8e
MayankRajeevkumarShukla <97381684+MayankRajeevkumarShukla@users.noreply.github.com>
2025-08-07 08:29:19 +0530

    serde

---
+++ try.rs
@@ -0,0 +1,18 @@
+use serde::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct Person {
+    name: String,
+    age: u8,
+}
+
+fn main() {
+    let person1 = Person {
+        name: String::from("Mayank"),
+        age: 22,
+    };
+
+    let person2 = person1.clone(); // Thanks to Clone trait
+
+    println!("{:?}", person2);     // Thanks to Debug trait
+}
